ccac61dc9a9c833d84456403ede77451be9ba141
Kellan Delaney <ked2pf@virginia.edu>
2021-09-27 23:36:40 -0400

    configured clock system and PWM

--- Debug/main.d
+++ Debug/main.d
@@ -1,8 +1,8 @@
 # FIXED
 
 main.obj: ../main.c
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/driverlib.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/driverlib.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
 main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h
 main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr2433.h
 main.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h
@@ -20,38 +20,39 @@ main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/mach
 main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/machine/_stdint.h
 main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h
 main.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdbool.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/rom_driverlib.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/rom_headers/rom_driverlib_fr253x_fr263x.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/lcd_e.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/ecomp.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/cs.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/sac.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/timer_b.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/framctl.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/tia.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/tlv.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/timer_a.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/pmm.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/sysctl.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/sfr.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/mpy32.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/wdt_a.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/icc.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/gpio.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/adc.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/crc.h
-main.obj: C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/rtc.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/rom_driverlib.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/rom_headers/rom_driverlib_fr253x_fr263x.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/lcd_e.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/ecomp.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/cs.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/sac.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/timer_b.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/framctl.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/tia.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/tlv.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/timer_a.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/pmm.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/sysctl.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/sfr.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/mpy32.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/wdt_a.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/icc.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/gpio.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/adc.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/crc.h
+main.obj: C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/rtc.h
+main.obj: ../Board.h
 
 ../main.c:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/driverlib.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/driverlib.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h:
 
 C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h:
 
@@ -87,55 +88,57 @@ C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/sys/_stdint.h:
 
 C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/stdbool.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/rom_driverlib.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/rom_driverlib.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/rom_headers/rom_driverlib_fr253x_fr263x.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/rom_headers/rom_driverlib_fr253x_fr263x.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/lcd_e.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/lcd_e.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/ecomp.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/ecomp.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/cs.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/cs.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/sac.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/sac.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/timer_b.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/timer_b.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/framctl.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/framctl.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/tia.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/tia.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/tlv.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/tlv.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/timer_a.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/timer_a.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/pmm.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/pmm.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/sysctl.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/sysctl.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/sfr.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/sfr.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/mpy32.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/mpy32.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/wdt_a.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/wdt_a.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/icc.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/icc.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/gpio.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/gpio.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/adc.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/adc.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/crc.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/crc.h:
 
-C:/Users/Student/Box/CapstoneDev/emptyProject/driverlib/MSP430FR2xx_4xx/rtc.h:
+C:/Users/Student/Box/CapstoneDev/MSP430/driverlib/MSP430FR2xx_4xx/rtc.h:
+
+../Board.h:
 
